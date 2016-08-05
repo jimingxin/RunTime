d842b00e9e8976a3b383403e211aadde1aa4a531
嵇明新 <493972613@qq.com>
2016-08-05 08:55:24 +0800

    Initial Commit

---
+++ RanTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RanTime
+//
+//  Created by 嵇明新 on 16/8/5.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
